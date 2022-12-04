6ccec72c23d2c780b9ae6a9c59adc9e01e0666d5
fabmars <fabmars@gmail.com>
2022-12-05 00:54:17 +0100

    2012-12-04

---
+++ src/main/java/aoc2022/Aoc20221204_2.java
@@ -0,0 +1,29 @@
+package aoc2022;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Aoc20221204_2 {
+
+  public static void main(String[] args) throws IOException {
+    int total = 0;
+    try(BufferedReader br = new BufferedReader(new InputStreamReader(Aoc20221204_2.class.getResourceAsStream("/1204-assign.txt")))) {
+      String line;
+      while((line = br.readLine()) != null) {
+        String[] parts = line.split(",");
+        String[] r1 = parts[0].split("-");
+        String[] r2 = parts[1].split("-");
+        int r1s = Integer.parseInt(r1[0]);
+        int r1e = Integer.parseInt(r1[1]);
+        int r2s = Integer.parseInt(r2[0]);
+        int r2e = Integer.parseInt(r2[1]);
+
+        if((r1s <= r2e && r1e >= r2s)) {
+          total++;
+        }
+      }
+    }
+    System.out.println(total);
+  }
+}
